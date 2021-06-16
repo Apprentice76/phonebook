0ad855560bb78c4105d80da3ca852643a6057ea6
Apprentice76 <hrk.som.01@gmail.com>
2021-06-16 14:08:54 +0530

    v1.1 heroku

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(errorHandler);
 
 const uri = 'mongodb+srv://client-side:asdfghjkl@cluster0.i4xit.mongodb.net/phonebook?retryWrites=true';
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const server = app.listen(PORT, () =>
     console.log(`Server running on port: ${PORT}`)
 );
